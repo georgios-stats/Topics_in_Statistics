524024e9bed4c644ce9804911f2fffea68ae48d5
georgios <georgios@caratheodory.Home>
2020-08-04 20:08:01 +0100

    info-8

--- edworth_ex/edworth_ex.R
+++ edworth_ex/edworth_ex.R
@@ -1,3 +1,31 @@
+#   <!-- -------------------------------------------------------------------------------- -->
+#   
+#   <!-- Copyright 2017 Georgios Karagiannis -->
+#   
+#   <!-- georgios.karagiannis@durham.ac.uk -->
+#   <!-- Assistant Professor -->
+#   <!-- Department of Mathematical Sciences, Durham University, Durham,  UK  -->
+#   
+#   <!-- This file is part of Topics in Statistics III/IV (MATH3361/4071) Michaelmas term 2018  -->
+#   <!-- which is the material of the course Topics in Statistics III/IV (MATH3361/4071) -->
+#   <!-- taught by Georgios P. Katagiannis in the Department of Mathematical Sciences   -->
+#   <!-- in the University of Durham  in Michaelmas term in 2018 -->
+#   
+#   <!-- Topics_in_Statistics_Michaelmas_2018 is free software: you8can redistribute it and/or modify -->
+#   <!-- it under the terms of the GNU General Public License as published by -->
+#   <!-- the Free Software Foundation version 3 of the License. -->
+#   
+#   <!-- Topics_in_Statistics_Michaelmas_2018 is distributed in the hope that it will be useful, -->
+#   <!-- but WITHOUT ANY WARRANTY; without even the implied warranty of -->
+#   <!-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the -->
+#   <!-- GNU General Public License for more details. -->
+#   
+#   <!-- You should have received a copy of the GNU General Public License -->
+#   <!-- along with Bayesian_Statistics  If not, see <http://www.gnu.org/licenses/>. -->
+#   
+#   <!-- -------------------------------------------------------------------------------- -->
+
+
 
 fntsz = 1.5 
 
